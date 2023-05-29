d7132e7ba8b5e9995cdfdf31df6621eeb2ecd291
vectorcrow <haider@contlo.com>
2023-05-29 15:58:04 +0530

    Minor bug fixes on docs build

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/',
               },
             ],
           },
@@ -116,10 +116,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
